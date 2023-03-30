5b75b044be89b6e4655f37b2f0d083168bfb9e51
Rychard Mazarin <rychardnbmazarin89@gmail.com>
2023-03-30 09:43:58 -0300

    Mudança no banco de dados online

--- my_community/__init__.py
+++ my_community/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = '50ef59056578f21b0daf78ca4d7b823e'
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///community.db'
+if os.getenv('DATABASE_URL'):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL')
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///community.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
